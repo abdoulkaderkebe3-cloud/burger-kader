a0fc7fda514e3035809441dc7c52c8af05abf3e1
kader <abdoulkaderkebe3@gmail.com>
2026-01-28 06:55:59 +0000

    v9

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -13,7 +13,6 @@ export default function HomePage() {
       <LastProducts/>
       <Products/>
       <ContenuePro/>
-      <div className="bg-red-500 w-full h-20 sm:bg-blue-500 md:bg-yellow-600 lg:bg-indigo-500 xl:bg-green-500 2xl:bg-pink-500"></div>
       <Event/>
       <Booking/>
       <Footer/>
